5bc26662ce1355a9ba2ea4766a4c312538f53041
Rene Milas <r.m.charlie@proton.me>
2026-01-31 14:17:38 +0100

    Fix: show uploaded images (hero slider + gallery)

---
+++ assets/media.js
@@ -0,0 +1,5 @@
+window.GG_MEDIA = {
+  BUILD: "1769865457",
+  hero: ["./img/hero/hero-01.jpg", "./img/hero/hero-02.jpg", "./img/hero/hero-03.jpg", "./img/hero/hero-04.png", "./img/hero/hero-05.jpg"],
+  gallery: ["./img/galerija/gal-01.jpg", "./img/galerija/gal-02.jpg", "./img/galerija/gal-03.jpg", "./img/galerija/gal-04.jpg", "./img/galerija/gal-05.jpg", "./img/galerija/gal-06.jpg", "./img/galerija/gal-07.png", "./img/galerija/gal-08.png", "./img/galerija/gal-09.png", "./img/galerija/gal-10.png"]
+};
